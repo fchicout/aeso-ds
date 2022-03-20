0370d69f80b91361620e278b483fefe2baeb37e0
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-03-20 01:09:33 -0300

    print everything else, except main diagonal

--- aula00/aula00-ex04/ex04.c
+++ aula00/aula00-ex04/ex04.c
@@ -54,6 +54,18 @@ int main(int argc, char const *argv[])
         }
     }
     
-
+    printf("\nprinting main diagonal\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i != j)
+            {
+                printf("%d ", mat[i][j]);
+            } else {
+                printf(" - ");
+            }
+        }
+    }
     return 0;
 }
