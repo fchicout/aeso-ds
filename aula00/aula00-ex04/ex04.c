f368b73f399b77020944b2760a9f0d2b9acd75b9
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-03-20 01:05:46 -0300

    code to show main and secondary diagonal

--- aula00/aula00-ex04/ex04.c
+++ aula00/aula00-ex04/ex04.c
@@ -15,6 +15,32 @@ int main(int argc, char const *argv[])
                      {1,2,3,4,5},
                      {1,2,3,4,5},
                      {1,2,3,4,5},
-                     {1,2,3,4,5}}
+                     {1,2,3,4,5}};
+
+    printf("printing main diagonal\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (i == j)
+            {
+                printf("%d ", mat[i][j]);
+            }
+        }
+    }
+    
+    printf("printing secondary diagonal\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (j == (5-i))
+            {
+                printf("%d ", mat[i][j]);
+            }
+        }
+    }
+    
+
     return 0;
 }
