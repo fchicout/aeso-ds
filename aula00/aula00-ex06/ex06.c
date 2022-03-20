51bc46ff9cb80df3f9cbeef372b7cdf280253414
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-03-20 01:43:01 -0300

    fixes on matrix dimensions and checking if it can really be multiplied

--- aula00/aula00-ex06/ex06.c
+++ aula00/aula00-ex06/ex06.c
@@ -2,9 +2,9 @@
 
 /*
 Faça um programa para multiplicar duas matrizes com tamanho até 10x10,
-armazenando o resultado em uma terceira matriz. 
+armazenando o resultado em uma terceira matriz.
  - O programa deve solicitar ao usuário as duas dimensões das duas matrizes;
- - O programa deve verificar se as matrizes podem ser multiplicadas e 
+ - O programa deve verificar se as matrizes podem ser multiplicadas e
    apresentar uma mensagem de erro, caso não seja possível.
 */
 
@@ -14,27 +14,36 @@ int main(int argc, char const *argv[])
     int matB[10][10];
     int matResult[10][10];
 
-    int xAxisSize=0, yAxisSize=0;
+    int matAxSize = 0, matAySize = 0, matBxSize = 0, matBySize = 0;
 
     // Ask user to tell me what are the dimensions of the matrix to multiply
-    printf("Qual a dimensão em x da matriz?\n");
-    scanf("%d", xAxisSize);
-    printf("\nQual a dimensão em y da matriz?\n");
-    scanf("%d", yAxisSize);
+    printf("Qual a dimensão em x da matriz A (less than 10))?\n");
+    scanf("%d", matAxSize);
+    printf("\nQual a dimensão em y da matriz A (less than 10)?\n");
+    scanf("%d", matAySize);
 
-    
+    printf("Qual a dimensão em x da matriz B (less than 10))?\n");
+    scanf("%d", matBxSize);
+    printf("\nQual a dimensão em y da matriz B (less than 10)?\n");
+    scanf("%d", matBySize);
 
-    int j = 0; //Second for loop counter initial value
-
-    for (int i = 0; i < xAxisSize; i++)
+    // Number of columns from A must be equal to the number of rows of B
+    if (matAySize == matBxSize)
     {
-        matResult[i][j] = 0; // Inicializando acumulador
-        for (; j < yAxisSize; j++)
+        int j = 0; // Second for loop counter initial value
+
+        for (int i = 0; i < 10; i++)
         {
-            matResult[i][j] += matA[i][j] * matB[i][j];
+            matResult[i][j] = 0; // Inicializando acumulador
+            for (; j < 10; j++)
+            {
+                matResult[i][j] += matA[i][j] * matB[i][j];
+            }
         }
+    } else {
+        printf("Number of columns from A must be equal to the number of rows of B. Cant multiply");
     }
-    
-    
+
+
     return 0;
 }
