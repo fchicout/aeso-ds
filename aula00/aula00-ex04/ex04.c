fe3f364cf093985a63fc803210fbc78497705739
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-03-20 00:58:07 -0300

    sample matrix creation

---
+++ aula00/aula00-ex04/ex04.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+Dada uma matriz 5x5, elabore um algoritmo que imprima:
+- A diagonal principal
+- A diagonal secundária
+- A soma da linha 4
+- A soma da coluna 2
+- Tudo, exceto a diagonal principal
+*/
+
+int main(int argc, char const *argv[])
+{
+    int mat[5][5] = {{1,2,3,4,5}, 
+                     {1,2,3,4,5},
+                     {1,2,3,4,5},
+                     {1,2,3,4,5},
+                     {1,2,3,4,5}}
+    return 0;
+}
