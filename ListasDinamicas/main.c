94b94935d74c404dbbf8604d2b96da14b990efa7
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-04-22 10:50:11 -0300

    removeNoLista implemented

--- ListasDinamicas/main.c
+++ ListasDinamicas/main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct lista
 {
     int info;
-    struct lista* prox;
+    struct lista *prox;
 };
 typedef struct lista Lista;
 
-Lista* criaLista(){
+Lista *criaLista()
+{
     return NULL;
 }
 
-Lista* insereNoLista(Lista* cursor, int valor){
-    Lista* novoNo = (Lista*) malloc(sizeof(Lista));
+Lista *insereNoLista(Lista *cursor, int valor)
+{
+    Lista *novoNo = (Lista *)malloc(sizeof(Lista));
     novoNo->info = valor;
     novoNo->prox = cursor;
     cursor = novoNo;
     return cursor;
 }
 
-void imprimir(Lista* inicio){
+void imprimir(Lista *inicio)
+{
     Lista *p;
     if (inicio == NULL)
     {
@@ -28,9 +32,30 @@ void imprimir(Lista* inicio){
     }
     for (p = inicio; p != NULL; p = p->prox)
     {
-        printf("%d ", p->info);
+        printf("%d \n", p->info);
     }
 }
+
+Lista* removeNoLista(Lista *ref, int valor)
+{
+    Lista *ant = NULL;
+    Lista *p = ref;
+    while (p != NULL && p->info != valor)
+    {
+        ant = p;
+        p = p->prox;
+    }
+    if (p == NULL) // elemento não encontrado
+        return ref;
+    // remove primeiro elemento
+    if (p == ref)
+        ref = p->prox;
+    else // remove do meio ou fim da lista
+        ant->prox = p->prox;
+    free(p);
+    return ref;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -38,6 +63,8 @@ int main(int argc, char const *argv[])
     noinicial = insereNoLista(noinicial, 5);
     noinicial = insereNoLista(noinicial, 9);
     noinicial = insereNoLista(noinicial, 36);
-    
+    imprimir(noinicial);
+    removeNoLista(noinicial, 9);
+    imprimir(noinicial);
     return 0;
 }
