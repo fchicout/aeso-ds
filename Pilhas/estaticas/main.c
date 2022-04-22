f1ee57e03ef2b7f3d0ee0794d614432595e10e55
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-04-22 19:30:22 -0300

    implemented stackSize function

--- Pilhas/estaticas/main.c
+++ Pilhas/estaticas/main.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
+/* 
+ * Criar uma pilha
+ * Empilhar (push)
+ * Desempilhar (pop)
+ * Tamanho (size)
+ * estaVazia (isEmpty)
+ * Liberar Pilha da memória (freeStack)
+ */
+
+struct stackItem
+{
+    int value;
+};
+typedef struct stackItem StackItem;
+
+struct stack
+{
+    StackItem items[10];
+    int stackSize;
+};
+typedef struct stack Stack;
+
+int stackSize(Stack s){
+    return s.stackSize;
+}
 
 
 
