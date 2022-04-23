34b77e0b9576cd68d35646a60a63c99a3e52fc69
FABIO CESAR FIGUEIREDO DE MEDEIROS CHICOUT <fabio.chicout@ufpe.br>
2022-04-22 21:39:44 -0300

    structs created

--- Pilhas/dinamicas/main.c
+++ Pilhas/dinamicas/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+struct task
+{
+    char* titulo;
+    char* responsavel;
+    struct task *upNext;
+};
+typedef struct task Task;
+
+typedef struct 
+{
+    Task* bottom, top;
+    int stackSize;
+} Stack;
+
+
+
 int main(int argc, char const *argv[])
 {
     /* code */
